01fd8cab8ff1f83481325e6e20172347ad34c2d9
Jinyoung Jang <jyjang@uengine.org>
2022-11-22 18:21:06 +0900

    commit

---
+++ car-app/src/main/java/carapp/infra/PolicyStatusViewHandler.java
@@ -0,0 +1,25 @@
+package carapp.infra;
+
+import carapp.domain.*;
+import carapp.config.kafka.KafkaProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PolicyStatusViewHandler {
+
+    @Autowired
+    private PolicyStatusRepository policyStatusRepository;
+
+
+
+
+
+}
+
