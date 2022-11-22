6786dcf73d59334bec47ccd995ac1c1fff4c3fc4
Jinyoung Jang <jyjang@uengine.org>
2022-11-22 09:35:17 +0000

    c

--- car/car/PolicyHistory.go
+++ car/car/PolicyHistory.go
@@ -2,22 +2,19 @@ package car
 
 import (
 	"gopkg.in/jeevatkm/go-model.v1"
-	
+
 	"gorm.io/gorm"
-	"fmt"
-	"car/external"
 )
 
 type PolicyHistory struct {
 	gorm.Model
-	Id int `gorm:"primaryKey" json:"id" type:"int"`
-	CarId int `json:"carId"`
-	PolicyApplicationId int `json:"policyApplicationId"`
-	Status string `json:"status"`
-
+	Id                  int    `gorm:"primaryKey" json:"id" type:"int"`
+	CarId               int    `json:"carId"`
+	PolicyApplicationId int    `json:"policyApplicationId"`
+	Status              string `json:"status"`
 }
 
-func (self *PolicyHistory) onPostPersist() (err error){
+func (self *PolicyHistory) onPostPersist() (err error) {
 	policyApplied := NewPolicyApplied()
 	model.Copy(policyApplied, self)
 
@@ -29,25 +26,24 @@ func (self *PolicyHistory) onPostPersist() (err error){
 
 	return nil
 }
-func (self *PolicyHistory) onPrePersist() (err error){ return nil }
-func (self *PolicyHistory) onPreUpdate() (err error){ return nil }
-func (self *PolicyHistory) onPostUpdate() (err error){ return nil }
-func (self *PolicyHistory) onPreRemove() (err error){ return nil }
-func (self *PolicyHistory) onPostRemove() (err error){ return nil }
+func (self *PolicyHistory) onPrePersist() (err error) { return nil }
+func (self *PolicyHistory) onPreUpdate() (err error)  { return nil }
+func (self *PolicyHistory) onPostUpdate() (err error) { return nil }
+func (self *PolicyHistory) onPreRemove() (err error)  { return nil }
+func (self *PolicyHistory) onPostRemove() (err error) { return nil }
 
-
-func AddPolicy(policyCreated *PolicyCreated){
-	/** Example 1:  new item
+func AddPolicy(policyCreated *PolicyCreated) {
+	/** Example 1:  new item	*/
 	policyHistory := &PolicyHistory{}
+	policyHistory.PolicyApplicationId = policyCreated.Id
 	policyHistoryrepository.save(policyHistory)
 
 	policyApplied := NewPolicyApplied()
-	model.Copy(policyApplied, self)
+	model.Copy(policyApplied, policyHistory)
 	Publish(policyApplied)
-	policyDenied := NewPolicyDenied()
-	model.Copy(policyDenied, self)
-	Publish(policyDenied)
-	*/
+	// policyDenied := NewPolicyDenied()
+	// model.Copy(policyDenied, self)
+	// Publish(policyDenied)
 
 	/** Example 2:  finding and process
 	id, _ := strconv.ParseInt(policyCreated.id, 10, 64)
